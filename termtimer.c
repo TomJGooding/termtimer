33a75f5de6f8985ce0c757c5ee66a009ffca2ef5
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-06-25 20:12:16 +0100

    refactor: lookup timer chars

--- termtimer.c
+++ termtimer.c
@@ -3,6 +3,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define TIMER_HEIGHT 1
+#define TIMER_WIDTH 8
+
+void display_timer(int remaining_seconds) {
+    int hours = remaining_seconds / 3600;
+    int minutes = (remaining_seconds % 3600) / 60;
+    int seconds = remaining_seconds % 60;
+
+    char digit_chars[11] = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':'
+    };
+
+    int timer_digits[8];
+    timer_digits[0] = hours / 10;
+    timer_digits[1] = hours % 10;
+    timer_digits[2] = 10;  // colon
+    timer_digits[3] = minutes / 10;
+    timer_digits[4] = minutes % 10;
+    timer_digits[5] = 10;  // colon
+    timer_digits[6] = seconds / 10;
+    timer_digits[7] = seconds % 10;
+
+    printf("\r");
+    // printf("%02d:%02d:%02d", hours, minutes, seconds);
+    for (size_t y = 0; y < TIMER_HEIGHT; y++) {
+        for (size_t x = 0; x < TIMER_WIDTH; x++) {
+            printf("%c", digit_chars[timer_digits[x]]);
+        }
+    }
+    fflush(stdout);
+}
+
 void show_cursor() {
     printf("\x1b[?25h");
 }
@@ -10,17 +42,6 @@ void show_cursor() {
 void hide_cursor() {
     printf("\x1b[?25l");
 }
-
-void display_timer(int remaining_seconds) {
-    int hours = remaining_seconds / 3600;
-    int minutes = (remaining_seconds % 3600) / 60;
-    int seconds = remaining_seconds % 60;
-
-    printf("\r");
-    printf("%02d:%02d:%02d", hours, minutes, seconds);
-    fflush(stdout);
-}
-
 void sigint_handler(int signo) {
     printf("\n");
     show_cursor();
